bd3b3166a13d3c0af5c35f10807e28b8bfcfc537
enginecode-cpu <bassmatics1002@gmail.com>
2021-09-29 21:05:30 +0900

    remove url

--- components/Json/Content.tsx
+++ components/Json/Content.tsx
@@ -1,32 +1,19 @@
 import React, { ChangeEvent } from 'react'
 import Editor, { OnChange } from '@monaco-editor/react'
-import { Input } from '@components/Bootstrap'
 
 type JsonContentProps = {
-  typeName: string
-  onTypeNameChange: (e: ChangeEvent<HTMLInputElement>) => void
   json: string
   onJsonChange: OnChange
   tsInterface: string
 }
 
 const JsonContent: React.FC<JsonContentProps> = ({
-  typeName,
-  onTypeNameChange,
   json,
   onJsonChange,
   tsInterface,
 }) => {
   return (
     <>
-      <Input
-        className="mb-3"
-        required
-        type="text"
-        value={typeName}
-        onChange={onTypeNameChange}
-        placeholder="Type Name"
-      />
       <Editor
         height="250px"
         defaultValue={json}
