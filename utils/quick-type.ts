6534282e07f5ba9bc597b2a3e30e22ab949bd074
enginecode-cpu <bassmatics1002@gmail.com>
2021-10-02 10:13:40 +0900

    필요없는 부분 삭제

--- utils/quick-type.ts
+++ utils/quick-type.ts
@@ -2,8 +2,6 @@ import {
   quicktype,
   InputData,
   jsonInputForTargetLanguage,
-  JSONSchemaInput,
-  FetchingJSONSchemaStore,
 } from "quicktype-core";
 
 export async function quicktypeJSON(
@@ -13,9 +11,6 @@ export async function quicktypeJSON(
 ) {
   const jsonInput = jsonInputForTargetLanguage(targetLanguage);
 
-  // We could add multiple samples for the same desired
-  // type, or many sources for other types. Here we're
-  // just making one type from one piece of sample JSON.
   await jsonInput.addSource({
     name: typeName,
     samples: [jsonString],
@@ -23,29 +18,6 @@ export async function quicktypeJSON(
 
   const inputData = new InputData();
   inputData.addInput(jsonInput);
-
-  return await quicktype({
-    inputData,
-    lang: targetLanguage,
-    rendererOptions: {
-      "just-types": "true",
-    },
-  });
-}
-
-export async function quicktypeJSONSchema(
-  targetLanguage: string,
-  typeName: string,
-  jsonSchemaString: string
-) {
-  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
-
-  // We could add multiple schemas for multiple types,
-  // but here we're just making one type from JSON schema.
-  await schemaInput.addSource({ name: typeName, schema: jsonSchemaString });
-
-  const inputData = new InputData();
-  inputData.addInput(schemaInput);
 
   return await quicktype({
     inputData,
