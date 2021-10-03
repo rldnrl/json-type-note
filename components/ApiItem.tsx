abfed5e62b0cc251c3c227fff81ade2e684f12e6
enginecode-cpu <bassmatics1002@gmail.com>
2021-10-03 16:52:53 +0900

    react fragment 삭제

--- components/ApiItem.tsx
+++ components/ApiItem.tsx
@@ -28,37 +28,35 @@ const ApiItemComponent: React.FC<ApiItemComponentProps> = ({
   typeName
 }) => {
   return (
-    <>
-      <div className="p-4">
-        <h1 className={titleStyle}>{typeName}</h1>
-        <Accordion.Item eventKey={eventKey}>
-          <Accordion.Header className="fs-2">
-            <InputGroup className="flex-grow-0 w-auto">
-              <Select
-                className={cx("flex-grow-0 w-auto", setOptionMethodColor(method))}
-                disabled
-                defaultValue={method}
-              >
-                <Option value="GET">GET</Option>
-                <Option value="POST">POST</Option>
-                <Option value="PUT">PUT</Option>
-                <Option value="PATCH">PATCH</Option>
-                <Option value="DELETE">DELETE</Option>
-              </Select>
-              <Select className="flex-grow-0 w-auto" disabled defaultValue={requestOrResponse}>
-                <Option value="Response">Response JSON</Option>
-                <Option value="Request">Payload JSON</Option>
-              </Select>
-            </InputGroup>
-          </Accordion.Header>
-          <Accordion.Body className="p-3 border rounded">
-            <Code language="json" code={json} />
-            <div className="my-4" />
-            <Code language="typescript" code={tsInterface} />
-          </Accordion.Body>
-        </Accordion.Item>
-      </div>
-    </>
+    <div className="p-4">
+      <h1 className={titleStyle}>{typeName}</h1>
+      <Accordion.Item eventKey={eventKey}>
+        <Accordion.Header className="fs-2">
+          <InputGroup className="flex-grow-0 w-auto">
+            <Select
+              className={cx("flex-grow-0 w-auto", setOptionMethodColor(method))}
+              disabled
+              defaultValue={method}
+            >
+              <Option value="GET">GET</Option>
+              <Option value="POST">POST</Option>
+              <Option value="PUT">PUT</Option>
+              <Option value="PATCH">PATCH</Option>
+              <Option value="DELETE">DELETE</Option>
+            </Select>
+            <Select className="flex-grow-0 w-auto" disabled defaultValue={requestOrResponse}>
+              <Option value="Response">Response JSON</Option>
+              <Option value="Request">Payload JSON</Option>
+            </Select>
+          </InputGroup>
+        </Accordion.Header>
+        <Accordion.Body className="p-3 border rounded">
+          <Code language="json" code={json} />
+          <div className="my-4" />
+          <Code language="typescript" code={tsInterface} />
+        </Accordion.Body>
+      </Accordion.Item>
+    </div>
   )
 }
 
