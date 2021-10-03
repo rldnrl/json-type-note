90c6c87133fcbcd2a4d7e2adfbbcade4aa8a2a9c
enginecode-cpu <bassmatics1002@gmail.com>
2021-10-03 16:57:00 +0900

    단축형으로 표현. isNull 추가

--- utils/type-guard.ts
+++ utils/type-guard.ts
@@ -1,7 +1,7 @@
-export const isUndefined = (value: unknown | undefined): value is undefined => {
-  return typeof value === "undefined";
-};
+export const isUndefined = (value: unknown | undefined): value is undefined =>
+  typeof value === "undefined";
 
-export const isString = (value: unknown | undefined): value is string => {
-  return typeof value === "string"
-}
+export const isString = (value: unknown | undefined): value is string =>
+  typeof value === "string";
+
+export const isNull = (value: unknown | null): value is null => value === null;
